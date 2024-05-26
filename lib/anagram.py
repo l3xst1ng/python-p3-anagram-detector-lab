a08807856246be61cdae9db0121888f1347bb606
l3xst1ng <howdy.martt@gmail.com>
2024-05-26 21:15:29 +0300

    Anagram lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,29 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if self.is_anagram(word):
+                matches.append(word)
+        return matches
+    
+    def is_anagram(self, word):
+        word = word.lower()
+        if len(word) != len(self.word):
+            return False
+        if word == self.word:
+            return False
+        for char in word:
+            if char not in self.word:
+                return False
+            if word.count(char) != self.word.count(char):
+                return False
+        return True
+
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
